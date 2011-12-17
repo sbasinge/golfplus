2fcee68e35ce6c39ae0c5bc46cfd744cc29e0416
U-dell8100\Scott <Scott@dell8100.(none)>
2011-12-17 09:33:31 -0500

    add cache manifest for mobile login page

--- src/main/java/com/basinc/golfminus/domain/Handicap_.java
+++ src/main/java/com/basinc/golfminus/domain/Handicap_.java
@@ -6,10 +6,10 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="Dali", date="2011-12-17T06:45:31.263-0500")
+@Generated(value="Dali", date="2011-12-17T09:24:38.551-0500")
 @StaticMetamodel(Handicap.class)
 public class Handicap_ extends BaseEntity_ {
+	public static volatile SingularAttribute<Handicap, User> user;
 	public static volatile SingularAttribute<Handicap, BigDecimal> handicap;
 	public static volatile SingularAttribute<Handicap, Date> date;
-	public static volatile SingularAttribute<Handicap, User> user;
 }
